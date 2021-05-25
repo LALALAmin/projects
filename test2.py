6da84a1a746c458758d0d91746513f4a8852fc92
LALALAmin <1625720302@qq.com>
2021-05-25 11:16:05 +0800

    用于测试添加的文件

--- test2.py
+++ test2.py
@@ -2,6 +2,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print(f'welcom to use {name} !')
+    print(f"测试git和pycharm如何使用！")
 
 
 
