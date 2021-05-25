1bebc99630f6e8abbe97387da8ed094ac52bc1b5
LALALAmin <1625720302@qq.com>
2021-05-25 10:52:09 +0800

    用于测试添加的文件

--- test2.py
+++ test2.py
@@ -1,6 +1,8 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'welcom to use {name} !')
+
 
 
 # Press the green button in the gutter to run the script.
